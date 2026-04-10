4ce05c203935606eab4381c39e7ce982f5836308
guillaumeehret <guillaume@dev-mind.fr>
2026-04-10 15:15:26 +0200

    feat: update link to load planning

--- backend/src/main/kotlin/org/mixit/infra/api/router/RedirectRouterConfig.kt
+++ backend/src/main/kotlin/org/mixit/infra/api/router/RedirectRouterConfig.kt
@@ -91,8 +91,8 @@ class RedirectRouterConfig(
                 GET("/docs/presse/leaflet/fr") {
                     ServerResponse.permanentRedirect(properties.doc.fr.press).build()
                 }
-                GET("/2026/docs/schedule") {
-                    ServerResponse.permanentRedirect(URI.create("https://drive.proton.me/urls/KRJ2X0ZBEC#jHs9eL3mASHT"))
+                GET("/2026/docs/schedule/update1") {
+                    ServerResponse.permanentRedirect(URI.create("https://drive.proton.me/urls/quoaiciQeAUk"))
                         .build()
                 }
 
